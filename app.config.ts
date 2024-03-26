b6f92fbaab99fe63ca451861064d7159addcaa2f
Loneless <14093936+LonelessCodes@users.noreply.github.com>
2024-03-26 03:34:40 +0100

    tja muss ja

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 // https://github.com/nuxt-themes/alpine/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Cutiemarx',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
@@ -19,16 +19,21 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: false,
+
+        // our github repository
+        repository: 'https://einsame.cloud',
+
+        text: 'Cutiemarx'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'lonelessart',
+      instagram: '',
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
